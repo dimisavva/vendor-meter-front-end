299b05515b65940aa08e3ce23e7a4b62ad8275b4
dimisavva <119276394+dimisavva@users.noreply.github.com>
2023-03-01 00:53:50 -0800

    add css to ChangePasswordForm

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -7,16 +7,24 @@ import styles from './Landing.module.css'
 // types
 import { User } from '../../types/models'
 
+// services
+import * as authService from '../../services/authService'
+
 interface LandingProps {
   user: User | null;
+  handleLogout: ()=> void;
 }
 
 const Landing = (props: LandingProps): JSX.Element => {
   const { user } = props
 
+  const handleDeleteAccount = async (): Promise<void> => {
+
+  }
+;
   return (
     <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'vendor. please log in'}</h1>
+      <h1>hello, <br/>{user ? user.name : 'please log in'}</h1>
       <img src={tools} alt="tools" />
     </main>
   )
